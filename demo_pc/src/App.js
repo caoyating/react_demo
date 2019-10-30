4e2518b619d24509a1474309705cfec81410584d
caoyating <316664681@qq.com>
2019-10-30 08:32:51 +0800

    第一次提交架子

--- demo_pc/src/App.js
+++ demo_pc/src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/less.less'
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {/* 123456 */}
+    <Button type="primary">Primary</Button>
+    <Button>Default</Button>
+    <Button type="dashed">Dashed</Button>
+    <Button type="danger">Danger</Button>
+    <Button type="link">Link</Button>
+  </div>
   );
 }
 
